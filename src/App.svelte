cdb9beb61b2785076366afee4451a1fe932dcd3d
Tom Black <tom.black06007@gmail.com>
2023-09-26 11:50:55 -0600

    Added a button to add some dynamics.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,24 @@
 <script>
 	export let name;
+	let beltColor = "blue";
+
+	const handleClick = () => {
+		if(beltColor === "orange"){
+			beltColor = "blue"
+		} 
+		else 
+		{
+			beltColor = "orange"
+		}
+
+	};
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<p>The belt color is: {beltColor}</p>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={handleClick}>Change the belt color!</button>
 </main>
 
 <style>
